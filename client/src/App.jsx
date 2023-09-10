8b6b205e4bfd7c275b166a9112f5488999847661
Tanjim <tanjimseo@gmail.com>
2023-09-10 22:55:00 +0530

    setting up plain vite project done

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from 'react'
+import './App.css'
+import toast from 'react-hot-toast'
+
+function App() {
+
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
